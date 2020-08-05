805dc27bc91a33229d97cb7a2b4a2d7b66fc096c
Emo <franceschini.emilio@gmail.com>
2020-08-05 15:18:11 -0300

    Otro arreglo de ruta

--- backend/environment.js
+++ backend/environment.js
@@ -4,6 +4,6 @@ environment.DB = "db_coloridospillos";
 environment.DB_URL = "mongodb://localhost:27017/";
 environment.EXPRESS_PORT = 80;
 
-environment.APP_BASE_PATH = "/home/backend/multicolorespillos/";
+environment.APP_BASE_PATH = "/home/multicolorespillos/backend/";
 
 module.exports = environment;
