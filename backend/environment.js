10f7e7c58d8dda35d1fc606bb2b9e5d25a0141d0
Emilio Franceschini <37306539+EmoPorEmilio@users.noreply.github.com>
2021-01-03 00:21:33 -0300

    Delete environment.js

--- backend/environment.js
+++
@@ -1,9 +0,0 @@
-var environment = {};
-
-environment.DB = "db_coloridospillos";
-environment.DB_URL = "mongodb://localhost:27017/";
-environment.EXPRESS_PORT = 80;
-
-environment.APP_BASE_PATH = "/home/multicolorespillos/backend/";
-
-module.exports = environment;
